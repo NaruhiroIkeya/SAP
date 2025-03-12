94e5b585b17af12aa332197cb3c3daa04c2d74eb
Naruhiro Ikeya <naruhiro.ikeya@beex-inc.com>
2025-03-12 15:21:23 +0900

    20250312

--- bin/hana_backup_snapshot.sh
+++ bin/hana_backup_snapshot.sh
@@ -21,14 +21,16 @@ set -ue
 #====================================================================================
 #* SET variables*
 # Cloud Environment
-CLOUD=AZURE
-#Parameter name for SSM of data and log volumes
-SSMPARAMDATAVOL=imdbmaster-hdb-datavolumes
-SSMPARAMLOGVOL=imdbmaster-hdb-logvolumes
-#Key-Name of hdbuserstore
-HDBUSERSTORE=SYSTEM
+CLOUD=AWS
 # HANA Snapshot backup ID
-SnapshotID=$1
+Hostname=$1
+SnapshotID=$2
+Comment=$3
+#Parameter name for SSM of data and log volumes
+SSMPARAMDATAVOL="$Hostname-hdb-datavolumes"
+SSMPARAMLOGVOL="$Hostname-hdb-logvolumes"
+#Key-Name of hdbuserstore
+HDBUSERSTORE=ZSYSTEM_BACKUP
 
 #* SET more variables automatically
 if [ "AWS" = $CLOUD ]; then
@@ -36,13 +38,16 @@ if [ "AWS" = $CLOUD ]; then
   instanceid=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id`
   az=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone`
   region=`echo $az | sed 's/[a-z]$//'`
-  sidadm=$(ps -C sapstart -o user |awk 'NR ==2' | awk '{print $1}')
+  sidadm=$(ps -C sapstart -o user | awk 'NR ==2' | awk '{print $1}')
+  instanceid=`aws ec2 describe-instances --filter "Name=tag:Name,Values=$Hostname" --region ap-northeast-1 | jq -r ".Reservations[].Instances[].InstanceId"`
 elif [ "AZURE" = $CLOUD ]; then
-  instanceid=`curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.name'`
-  resourcegroup=`curl -s -H Metadata:true --noProxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.resourceGroupName'`
-  region=`curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.location'`
-  subscriptionId=`curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.subscriptionId'`
-  target_disk_name_list=`curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.storageProfile.dataDisks[].name'`       else
+  instanceid=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.name')
+  resourcegroup=$(curl -s -H Metadata:true --noProxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.resourceGroupName')
+  region=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.location')
+  subscriptionId=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.subscriptionId')
+  target_disk_name_list=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.storageProfile.dataDisks[].name')
+  sidadm=$(ps -C sapstart -o user | awk 'NR ==3' | awk '{print $1}')
+else
   echo "CLOUD Environment: 'AWS' or 'AZURE' or 'GCP??' "
   exit 1
 fi
@@ -51,7 +56,7 @@ fi
 logfile="/var/log/user-data.log"
 
 # Get status of first Tenant DB. status online=yes
-# status=$(sudo -u $sidadm -i hdbsql -U $HDBUSERSTORE "SELECT ACTIVE_STATUS from M_DATABASES;" | awk 'FNR==3' |sed 's/\"//g')
+status=$(sudo -u $sidadm -i hdbsql -U $HDBUSERSTORE "SELECT ACTIVE_STATUS from M_DATABASES;" | awk 'FNR==3' | sed 's/\"//g')
 
 #********************** Function Declarations ************************************************************************
 
@@ -61,7 +66,6 @@ log() {
 }
 
 # Function: Prerequisite check - check if HANA DB and tenant is online.
-status="YES"
 prerequisite_check() {
 if [ $status == "YES" ]; then
   log "INFO: HANA and tenant DB is online and ready for backup"
@@ -75,7 +79,7 @@ fi
 # Function: Create snapshot.
 snapshot_instance() {
   snapshot_suffix="-HANA-Snapshot-$(date +%Y%m%d%H%M%S)"
-  snapshot_description="$(hostname)-$instanceid-HANA-Snapshot-$(date +%Y-%m-%d-%H:%M:%S)"
+  snapshot_description="$Hostname-$instanceid-HANA-Snapshot-$(date +%Y-%m-%d-%H:%M:%S)"
 
   if [ "AWS" = $CLOUD ]; then
     recent_snapshot_list_new=($(aws ec2 create-snapshots --region $region --instance-specification InstanceId=$instanceid,ExcludeBootVolume=true --description $snapshot_description --tag-specifications "ResourceType=snapshot,Tags=[{Key=Createdby,Value=AWS-HANA-Snapshot_of_${HOSTNAME}}]" | jq -r ".Snapshots[] | .SnapshotId"))
@@ -109,8 +113,8 @@ tag_mountinfo() {
       device_name_sorted+=($(aws ec2 describe-volumes --region $region --output=text --volume-ids ${volume_id_sorted[$i]} --query 'Volumes[0].{Devices:Attachments[0].Device}'))
 
       #add tag to snapshot
-      aws ec2 create-tags --region $region --resource ${recent_snapshot_list_new[$i]} --tags Key=device_name,Value=${device_name_sorted[$i]}
-      
+      aws ec2 create-tags --region $region --resource ${recent_snapshot_list_new[$i]} --tags Key=device_name,Value=${device_name_sorted[$i]} Key=hana_snapshot_id,Value=${SnapshotID} Key=comment,Value=${Comment}
+
     done
     IFS=$OIFS;
   elif [ "AZURE" = $CLOUD ]; then
@@ -122,23 +126,23 @@ tag_mountinfo() {
   fi
 }
 
-delete_invalid_snap() {
-  SNAP_VALIDATION=$(sudo -u $sidadm -i hdbsql -U $HDBUSERSTORE "select STATE_NAME from m_backup_catalog where ENTRY_TYPE_NAME = 'data snapshot' order by SYS_START_TIME desc limit 1" | awk 'FNR==2' |sed 's/\"//g')
-  if [ $SNAP_VALIDATION == "successful" ]; then
-    log "INFO: Snapshot successful, keep AWS snapshot"
-  else
-    if [ "AWS" = $CLOUD ]; then
-      for((i=0; i<${#recent_snapshot_list_new[@]}; ++i));  do
-        aws ec2 delete-snapshot --region $region --snapshot_id ${recent_snapshot_list_new[$i]}
-        log "ERROR: Backup was not marked as successful in HANA backup catalog - delete EBS snapshot: $recent_snapshot_id"
-      done
-    elif [ "AZURE" = $CLOUD ]; then
-      return 0
-    else
-      return 1
-    fi
-  fi
-}
+#delete_invalid_snap() {
+#  SNAP_VALIDATION=$(sudo -u $sidadm -i hdbsql -U $HDBUSERSTORE "select STATE_NAME from m_backup_catalog where ENTRY_TYPE_NAME = 'data snapshot' order by SYS_START_TIME desc limit 1" | awk 'FNR==2' | sed 's/\"//g')
+#  if [ $SNAP_VALIDATION == "successful" ]; then
+#    log "INFO: Snapshot successful, keep AWS snapshot"
+#  else
+#    if [ "AWS" = $CLOUD ]; then
+#      for((i=0; i<${#recent_snapshot_list_new[@]}; ++i));  do
+#        aws ec2 delete-snapshot --region $region --snapshot-id ${recent_snapshot_list_new[$i]}
+#        log "ERROR: Backup was not marked as successful in HANA backup catalog - delete EBS snapshot: ${recent_snapshot_list_new[$i]}"
+#      done
+#    elif [ "AZURE" = $CLOUD ]; then
+#      return 0
+#    else
+#      return 1
+#    fi
+#  fi
+#}
 
 #**********************************************************************************************************************************
 ### Call functions in required order
@@ -168,10 +172,10 @@ else log "ERROR: Could not create tags for EBS snapshots"
 fi
 
 # 6) Validate snap
-if delete_invalid_snap; then :
-else log "ERROR: EBS snapshots could not be deleted - please remove invalid snapshots manually"
-   exit 1
-fi
+#if delete_invalid_snap; then :
+#else log "ERROR: EBS snapshots could not be deleted - please remove invalid snapshots manually"
+#   exit 1
+#fi
 
 log "INFO: End of AWS snapshot backup"
 
--- bin/hana_delete_snapshot.sh
+++ bin/hana_delete_snapshot.sh
@@ -2,35 +2,87 @@
 set -ue
 #set -x
 
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
+#====================================================================================
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this
-# software and associated documentation files (the "Software"), to deal in the Software
-# without restriction, including without limitation the rights to use, copy, modify,
-# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 #====================================================================================
 #* SET variables*
-#Parameter name for SSM of data and log volumes
-SSMPARAMDATAVOL=imdbmaster-hdb-datavolumes
-SSMPARAMLOGVOL=imdbmaster-hdb-logvolumes
+# Cloud Environment
+CLOUD=AWS
 #Key-Name of hdbuserstore
-HDBUSERSTORE=SYSTEM
+HDBUSERSTORE=ZSYSTEM_BACKUP
+# HANA Snapshot backup ID
+SnapshotID=$1
 
 #* SET more variables automatically
-instanceid=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-az=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
-region="`echo \"$az\" | sed 's/[a-z]$//'`"
-sidadm=$(ps -C sapstart -o user |awk 'NR ==2' | awk '{print $1}')
+if [ "AWS" = $CLOUD ]; then
+  TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+  instanceid=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id`
+  az=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone`
+  region=`echo $az | sed 's/[a-z]$//'`
+elif [ "AZURE" = $CLOUD ]; then
+  instanceid=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.name')
+  resourcegroup=$(curl -s -H Metadata:true --noProxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.resourceGroupName')
+  region=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.location')
+  subscriptionId=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.subscriptionId')
+  target_disk_name_list=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.storageProfile.dataDisks[].name')
+else
+  echo "CLOUD Environment: 'AWS' or 'AZURE' or 'GCP??' "
+  exit 1
+fi
 
 #Log output to AWS console log
 logfile="/var/log/user-data.log"
+
+#********************** Function Declarations ************************************************************************
+
+# Function: Log an event.
+log() {
+  echo "[$(date +"%Y-%m-%d"+"%T")]: $*"
+}
+
+delete_snapshot() {
+  if [ "AWS" = $CLOUD ]; then
+    snapshot_ids+=($(aws ec2 describe-snapshots --region $region --filters Name=tag:hana_snapshot_id,Values=${SnapshotID} | jq -r ".Snapshots[] | .SnapshotId"))
+    for((i=0; i<${#snapshot_ids[@]}; ++i));  do
+      aws ec2 delete-snapshot --region $region --snapshot-id ${snapshot_ids[$i]}
+      log "ERROR: Backup was not marked as successful in HANA backup catalog - delete EBS snapshot: ${snapshot_ids[$i]}"
+    done
+  elif [ "AZURE" = $CLOUD ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+#**********************************************************************************************************************************
+### Call functions in required order
+log "INFO: Start AWS remove snapshot backup"
+
+#log_setup
+log "INFO: Check log file $logfile for more information"
+log "INFO: HANA removable for Snapshot -- HANA Snapshot-ID: " $SnapshotID
+
+# 1) Delete snap
+if delete_snapshot; then :
+else log "ERROR: EBS snapshots could not be deleted - please remove invalid snapshots manually"
+   exit 1
+fi
+
+log "INFO: End of AWS snapshot deleted"
+
+exit 0
+#################################################################End of script ########################################
